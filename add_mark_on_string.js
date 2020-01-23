4c0bf871aadc74f9454cd8062bd9476f1aebfb2f
lgdelacruz <lgdelacruz@lgdelacruz.com>
2020-01-23 13:49:15 -0800

    Add marking

---
+++ add_mark_on_string.js
@@ -0,0 +1,51 @@
+const test = "Hello hello world hello";
+
+const adjustIndex = (i, arrayIndex) => {
+  return i + arrayIndex * 13;
+};
+
+const replaceAt = (str, at, match) => {
+  return (
+    str.substring(0, at) +
+    `<mark>${match}</mark>` +
+    str.substring(at + match.length)
+  );
+};
+
+const markHello = (str, searchTerm) => {
+  const MATCHER_LENGTH = 5;
+  const regExp = new RegExp(searchTerm, "g");
+  const matches = [...str.matchAll(regExp)];
+
+  const openingTagIndices = matches.map(i => i.index);
+  const closingTagIndices = matches.map(i => i.index + MATCHER_LENGTH);
+  let currentTagIndex = 0;
+  let markCount = 0;
+  let closeMarkCount = 0;
+
+  let result = [];
+  for (let i = 0; i < str.length; i++) {
+    const openTagIndex = openingTagIndices[currentTagIndex];
+    const closingTagIndex = closingTagIndices[currentTagIndex];
+
+    if (i === openTagIndex) {
+      result.push("<mark>");
+      markCount += 1;
+    } else if (i === closingTagIndex) {
+      result.push("</mark>");
+      currentTagIndex += 1;
+      closeMarkCount += 1;
+    }
+    result.push(str[i]);
+  }
+
+  if (markCount - closeMarkCount === 1) {
+    result.push("</mark>");
+  }
+
+  return result.join("");
+};
+
+const result = markHello(test, "hello");
+console.log(result);
+// console.log(replaceAt(test, 0, "Hello"));
